cc38211b5a1c1adbf6de5b8db4b44242868f9c99
Tulio Rodrigues <tulio.g.rodrigues@gmail.com>
2015-10-26 23:45:09 -0200

    Initial commit

---
+++ src/main/java/com/tgr/gateway/ProxyApplication.java
@@ -0,0 +1,16 @@
+package com.tgr.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
+public class ProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyApplication.class, args);
+    }
+}
