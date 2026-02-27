295c3ab36cb513deccd09e1f30986bd6835ca3f2
Tulio Rodrigues <tulio.g.rodrigues@gmail.com>
2026-02-27 11:21:42 +0100

    refactor: upgrade application to use javs 25 and spring-cloud.
    Upgrade this project to use a modern solution based on spring-cloud

--- src/main/java/com/tgr/gateway/GatewayApplication.java
+++ src/main/java/com/tgr/gateway/GatewayApplication.java
@@ -1,18 +1,27 @@
 package com.tgr.gateway;
 
+
+
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
-@EnableZuulProxy
-@EnableEurekaServer
-@EnableDiscoveryClient
 public class GatewayApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(GatewayApplication.class).web(true).run(args);
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+            .route(p -> p.path("/get")
+                .filters(f -> f.addRequestHeader("HELLO", "World"))
+                .uri("http://httpbin.org:80"))
+            .build();
     }
 }
