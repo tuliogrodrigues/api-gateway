ed6389f4a1e7139c52fb81d83f3daa67b7ed723e
Tulio Rodrigues <tulio.g.rodrigues@gmail.com>
2026-04-10 14:14:47 +0200

    refactor: upgrade spring-cloud version and add new gateway route configuration

--- src/main/java/com/tgr/gateway/GatewayApplication.java
+++ src/main/java/com/tgr/gateway/GatewayApplication.java
@@ -1,27 +1,31 @@
 package com.tgr.gateway;
 
-
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
 @SpringBootApplication
 public class GatewayApplication {
 
-    public static void main(String[] args) {
+    private static final Logger log = LoggerFactory.getLogger(GatewayApplication.class);
+
+    static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
     @Bean
     RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
-            .route(p -> p.path("/get")
-                .filters(f -> f.addRequestHeader("HELLO", "World"))
-                .uri("http://httpbin.org:80"))
-            .build();
+                .route("users", r ->
+                    r.path("/users", "/users/**")
+                            .filters(f -> f.rewritePath("/users", "/anything/users"))
+                            .uri("http://httpbin.org"))
+                .build();
     }
-}
+ }
